c32dfd12df5ca8cda06d3112181adffc0843e9b4
skim658 <33474168+seankim658@users.noreply.github.com>
2023-05-23 01:07:00 -0400

    init

---
+++ src/calc_angle_distance.py
@@ -0,0 +1,7 @@
+
+
+
+'''
+This script calculates a couple supplementary statistics using the X and y shot coordinates. The first statistic calculated
+is shot angle, the second statistic calculated is shot distance. 
+'''
