9f8d8455fb39afb17196a1e6345eac05e0a0485c
skim658 <33474168+seankim658@users.noreply.github.com>
2023-05-25 00:51:05 -0400

    working on database connection class

--- src/database_connection.py
+++ src/database_connection.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd 
 from sqlalchemy import create_engine
 from sqlalchemy import text 
+import tempfile
 
 class DBConn: 
     ''' Class for the database connection. 
@@ -12,17 +13,17 @@ class DBConn:
         Connection string for sqlalchemy. 
     engine : sqlalchemy.engine.Engine 
         Provides the database connectivity. 
-    table : str 
-        Table to query from. 
 
     Methods
     -------
-    __init__(host, user, port, db, password, table)
+    __init__(host, user, port, db, password)
         Constructor. 
     
+    query(query_str)
+        Query a table in the database. 
     '''
 
-    def __init__(self, host: str = 'localhost', user: str = 'db_user', port: int = 5438, db: str = 'shot_db', password: str = 'LetMeIn', table: str = 'shot_data_table') -> None:
+    def __init__(self, host: str = 'localhost', user: str = 'db_user', port: int = 5438, db: str = 'shot_db', password: str = 'LetMeIn') -> None:
         ''' Constructor. 
 
         Parameters
@@ -43,6 +44,48 @@ class DBConn:
     
         self._connection_string = f'postgresql://{user}:{password}@{host}:{port}/{db}'
         self.engine = create_engine(self._connection_string)
-        self.table = table 
+
+    def query(self, query_str: str) -> pd.DataFrame:
+        ''' Query a table in the database using SQL. 
+
+        Parameters
+        ----------
+        query_str : str 
+            SQL query to execute. 
+
+        Returns 
+        -------
+        pd.Dataframe 
+            Result of the SQL query in the form of a pandas dataframe. 
+        '''
+
+        with self.engine.connect() as connection:
+            result = connection.execute(text(query_str))
+        # TODO
+        df = pd.DataFrame(result.fetchall())
+
+        return df
+    
+    def query_with_copy(self, query_str: str) -> pd.DataFrame:
+        ''' For large queries such as SELECT * FROM [table].
+
+        By using a temporary file to run a large query, we can 
+        avoid some of the memory overhead and constraints from 
+        pandas built-in 'read_sql' function. 
+
+        Parameters
+        ----------
+        query_str : str
+            SQL query to execute
+
+        Returns
+        -------
+        pd.Dataframe
+            Result of the SQL query in the form of a pandas dataframe. 
+        '''
+
+        # TODO 
+        pass 
+
 
     
